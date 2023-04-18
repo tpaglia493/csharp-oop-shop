e813867c91f8ded06c6628f5fe1c0fac5e9d01c9
tpaglia493 <tommasopaglia93@outlook.it>
2023-04-18 14:53:24 +0200

    Aggiungere i file di progetto.

---
+++ csharp-oop-shop/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    public class Product
+    {
+        //ATTRIBUTES
+
+        //STATES
+
+        //GETTERS
+
+        //SETTERS
+
+        //METHODS
+    }
+}
